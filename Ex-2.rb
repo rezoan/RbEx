c233c6a471f4bd2338d4daa1ca682e57342f409d
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2020-04-26 16:46:26 -0600

    Add files via upload

---
+++ Ex-2.rb
@@ -0,0 +1,8 @@
+#Something Something Comments
+# I'm Invisible
+puts "I could have code like this."
+
+#something something moar comments
+#also they disables code, for "testing" or something
+#puts "This wont run."
+puts "This will run."
---
+++ Ex-5.rb
@@ -0,0 +1,19 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usualy #{my_teeth} depending on the coffee."
+
+# It's tricky to write this line, to write this line, it's tricky. . . 
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
